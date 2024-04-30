4c1980950a168be74faf56345fed3674b1d8ed50
Nabila Bunon <jtajin09@gmail.com>
2024-04-30 21:46:47 +0600

    Add files via upload

---
+++ alpha.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    prinff("enter the two num %d%d:",a,b);
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("the sum is %d",sum);
+    return 0;
+}
